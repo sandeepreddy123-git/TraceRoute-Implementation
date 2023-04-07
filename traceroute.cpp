afb218c2dfbbe620dcafc94c1d24efe51397ebfa
sandeepreddy123-git <62788485+sandeepreddy123-git@users.noreply.github.com>
2023-04-07 15:19:57 -0400

    Update traceroute.cpp

--- traceroute.cpp
+++ traceroute.cpp
@@ -1,12 +1,12 @@
-#include <sys/socket.h>
-#include <resolv.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/icmp6.h>
 #include <arpa/inet.h>
 #include <endian.h>
 #include <iostream>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <thread>
 #include <unistd.h>
 
@@ -17,3 +17,24 @@
 #define DEFAULT_TTL            30       // default timeout
 
 using namespace std;
+
+typedef struct icmp_hdr
+{
+    unsigned char   icmp_type;       // Type of ICMP message (e.g. echo request, echo reply, etc.)
+    unsigned char   icmp_code;       // ICMP code (specific to each message type)
+    unsigned short  icmp_checksum;   // Checksum to ensure message integrity
+    unsigned short  _icmp_id;        // ICMP identifier field (usually set to process ID)
+    unsigned short  icmp_sequence;   // ICMP sequence number (used to match echo requests and replies)
+    unsigned int    icmp_timestamp;  // ICMP timestamp (used to calculate RTT)
+} ICMP_HDR;
+
+
+/*variabes to be used*/
+char gDestination[256] = { 0 };      
+char var[1] = { };
+int gAddressFamily = AF_UNSPEC;     
+int gProtocol = IPPROTO_ICMP;       
+int s;
+int packetlen;
+struct addrinfo *dest;
+struct addrinfo *local;

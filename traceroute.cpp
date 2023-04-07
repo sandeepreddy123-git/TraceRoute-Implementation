dd0a2617936d14ec52c12f52e9322a51bf522346
sandeepreddy123-git <62788485+sandeepreddy123-git@users.noreply.github.com>
2023-04-07 15:14:06 -0400

    Create traceroute.cpp

---
+++ traceroute.cpp
@@ -0,0 +1,19 @@
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/icmp6.h>
+#include <arpa/inet.h>
+#include <endian.h>
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+#define REQUEST_TYPE   8    // used to send ICMP echo requests to host
+#define REQUEST_CODE   0
+#define ICMPV4_ECHO           8
+#define SIZE_OF_DATA      32       // defaut size_of_data
+#define DEFAULT_TTL            30       // default timeout
+
+using namespace std;
